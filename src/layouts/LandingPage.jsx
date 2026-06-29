cb121226cd888708841e55a4d4c0bf82abc8ec43
SOUVIK MANDAL <souvikmandal@SOUVIKs-MacBook-Air.local>
2026-06-29 12:53:05 +0530

    cleaned up unused componentes

--- src/layouts/LandingPage.jsx
+++ src/layouts/LandingPage.jsx
@@ -2,20 +2,15 @@ import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Highlights from "../components/Highlights/Highlights";
 import WhatsAppButton from "../components/WhatsAppButton/WhatsAppButton";
-import About from "../components/About/About";
 import Principal from "../components/Principal/Principal";
 import Budgets from "../components/Budgets/Budgets";
 import VideoReview from "../components/VideoReview/VideoReview";
 import VirtualTour from "../components/VirtualTour/VirtualTour";
 import Solutions from "../components/Solutions/Solutions";
-import Services from "../components/Services/Services";
 import Process from "../components/Process/Process";
-import Stats from "../components/Stats/Stats";
 import FeaturedProjects from "../components/FeaturedProjects/FeaturedProjects";
 import Testimonials from "../components/Testimonials/Testimonials";
 import VideoTestimonials from "../components/VideoTestimonials/VideoTestimonials";
-import CTA from "../components/CTA/CTA";
-import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
 import data from "../data/mockData.json";
@@ -28,8 +23,6 @@ const LandingPage = () => {
       <Hero data={data.hero} />
 
       <Highlights data={data.highlights} />
-
-      {/* <About data={data.about} /> */}
 
       <Principal data={data.principal} />
 
@@ -49,7 +42,7 @@ const LandingPage = () => {
 
       <VideoTestimonials data={data.videoTestimonials} />
 
-      <Footer brand={data.brand} nav={data.nav} footer={data.footer} />
+      <Footer brand={data.brand} footer={data.footer} />
 
       <WhatsAppButton
         phone={data.contact.whatsapp}
